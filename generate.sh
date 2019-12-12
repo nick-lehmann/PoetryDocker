ff13a9b01760d2e5ba362cdea68921e780be171b
Nick Lehmann <nick@lehmann.sh>
2019-12-12 23:13:29 +0100

    Differentiate between build and runtime dependencies

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ls | grep "[0-9].[0-9]" | xargs rm -rf
-docker run -v $(pwd):/opt -w /opt fabric8/fish-pepper
+docker run -v $(pwd):/opt -w /opt --user $(id -u):$(id -g) fabric8/fish-pepper
 mv poetry/images/* .
 rmdir poetry/images
